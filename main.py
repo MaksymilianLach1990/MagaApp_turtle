a0352889b2c24531e9ea062ba7a7b4b5d4de5e44
Krzysztof Lach <lachkrzysztof56@gmail.com>
2022-03-31 21:07:34 +0200

    Create General Menu. Set Cursor to start position

--- main.py
+++ main.py
@@ -1,55 +1,84 @@
 import turtle
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
 # x szerokość
 # y wysokośc
+
+# Basic Variable
 xWindow = 500
 yWindow = 800
+startPoint = (-230, 320)
+items = ["Apple", "Orange", "Tant", "Dog", "Cat"]
+page = "Ap"
 
+# Window
 window = turtle.Screen()
 window.title("MagaApp")
 window.bgcolor("black")
 window.setup(xWindow, yWindow, startx=0, starty=0)
 window.tracer()
 
-
+# Cursor
 cursor = turtle.Turtle()
 cursor.speed(0)
 cursor.shape("triangle")
 cursor.color("white")
 cursor.penup()
-cursor.goto(-250, 380)
+cursor.goto(-250, 340)
 
+# Pen for the text
 pen = turtle.Turtle()
 pen.speed()
 pen.color("white")
 pen.penup()
 pen.hideturtle()
-pen.goto(-250, 380)
+pen.goto(-240, 380)
 
+# General Menu
 def generalMenu():
-    pen.goto()
+    gMenu = ["Items", "Settings", "Containers", "Exit"]
+    logging.debug("Print General Menu")
+    # Print page title
+    pen.penup()
+    pen.goto(-50, (yWindow/2)-40)
+    pen.write("Menu Główne", font=("Arial", 20, "bold"))
+
+    # Print elements
+    logging.debug("Print Elements from gMenu")
+    pen.goto(startPoint)
+    for element in gMenu:
+        pen.write(element, font=("Ariel", 20, "bold"))
+        y = pen.ycor()
+        y -= 40
+        pen.sety(y)
+
 
 def moveCursorUp():
-    logging.info(f"Cursor Y = {cursor.ycor()}")
+    logging.debug(f"Cursor from Y = {cursor.ycor()}")
     y = cursor.ycor()
-    if y >= 380:
+    if y >= 340:
         return
-    y += 20
-    logging.info(f"Cursor Y = {cursor.ycor()}")
+    y += 40
     cursor.sety(y)
+    logging.debug(f"Cursor to Y = {cursor.ycor()}")
 
 def moveCursorDown():
-    logging.info(f"Cursor Y = {cursor.ycor()}")
+    logging.debug(f"Cursor from Y = {cursor.ycor()}")
     y = cursor.ycor()
     if y <= -380:
         return
-    y -= 20
-    logging.info(f"Cursor Y = {cursor.ycor()}")
+    y -= 40
     cursor.sety(y)
+    logging.debug(f"Cursor to Y = {cursor.ycor()}")
+
+def showItems(items):
+    pen.penup()
+    pen.goto()
+    for item in items:
+        pen.write(item, font=("Arial", 20))
 
 # Keyboard binding
 window.listen()
@@ -57,7 +86,24 @@ window.onkeypress(moveCursorUp, "Up")
 window.onkeypress(moveCursorDown, "Down")
 
 
+logging.info("Loop start")
+generalMenu()
+# showItems(items, (-230, 350))
 while True:
     window.update()
+
+    if page == "General Menu":
+        generalMenu()
+        page = "Pause"
+
+    
+
+    
+
+
+
+
+
+
 
 
